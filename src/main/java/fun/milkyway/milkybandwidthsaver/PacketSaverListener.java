e76c3e91fb1ba1c41ca3f0c6c5ad4f56c3ed9199
radiant-ai <bodia1206@gmail.com>
2022-07-25 21:40:41 -0500

    add teleport suppression for minecarts, replaced entity rel move and look with just rel move

--- src/main/java/fun/milkyway/milkybandwidthsaver/PacketSaverListener.java
+++ src/main/java/fun/milkyway/milkybandwidthsaver/PacketSaverListener.java
@@ -1,21 +1,31 @@
 package fun.milkyway.milkybandwidthsaver;
 
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.PacketEventsAPI;
 import com.github.retrooper.packetevents.event.PacketListenerAbstract;
 import com.github.retrooper.packetevents.event.PacketListenerPriority;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.wrapper.play.server.*;
 import io.github.retrooper.packetevents.util.SpigotReflectionUtil;
-import org.bukkit.entity.FishHook;
-import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.*;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PacketSaverListener extends PacketListenerAbstract {
     private final MilkyBandwidthSaver plugin;
+    private final PacketEventsAPI<?> packetEventsAPI;
+    private final Map<Integer,AtomicInteger> cartCounters;
 
     public PacketSaverListener(MilkyBandwidthSaver plugin) {
         super(PacketListenerPriority.HIGHEST);
         this.plugin = plugin;
+        this.packetEventsAPI = PacketEvents.getAPI();
+        this.cartCounters = new ConcurrentSkipListMap<>();
+        startMapClearSchedule();
     }
 
     @Override
@@ -58,11 +68,8 @@ public class PacketSaverListener extends PacketListenerAbstract {
             var entity = SpigotReflectionUtil.getEntityById(entityId);
             if (!(entity instanceof Player)) {
                 var newPacket = new WrapperPlayServerEntityRelativeMove(entityId, wrapper.getDeltaX(), wrapper.getDeltaY(), wrapper.getDeltaZ(), wrapper.isOnGround());
-                try {
-                    event.setByteBuf(newPacket.buffer);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                packetEventsAPI.getPlayerManager().sendPacketSilently(player, newPacket);
+                event.setCancelled(true);
                 return;
             }
         }
@@ -71,10 +78,29 @@ public class PacketSaverListener extends PacketListenerAbstract {
             var wrapper = new WrapperPlayServerEntityVelocity(event);
             var entityId = wrapper.getEntityId();
             var entity = SpigotReflectionUtil.getEntityById(entityId);
-            if (!(entity instanceof Player) || !(entity instanceof FishHook) || !(entity instanceof Item)) {
+            if (!(entity instanceof Player || entity instanceof Projectile || entity instanceof Item)) {
                 event.setCancelled(true);
                 return;
             }
+        }
+
+        if (event.getPacketType() == PacketType.Play.Server.ENTITY_TELEPORT) {
+            var wrapper = new WrapperPlayServerEntityTeleport(event);
+            var entityId = wrapper.getEntityId();
+            var entity = SpigotReflectionUtil.getEntityById(entityId);
+
+            if (!(entity instanceof Minecart)) {
+                return;
+            }
+
+            var counter = cartCounters.computeIfAbsent(entityId, (id) -> new AtomicInteger(0));
+
+            if (counter.incrementAndGet() % 2 == 0) {
+                return;
+            }
+
+            event.setCancelled(true);
+            return;
         }
 
         if (event.getPacketType() == PacketType.Play.Server.ENTITY_EFFECT) {
@@ -149,28 +175,12 @@ public class PacketSaverListener extends PacketListenerAbstract {
 
         if (event.getPacketType() == PacketType.Play.Server.UPDATE_SCORE) {
             event.setCancelled(true);
-            return;
         }
     }
 
-    /*
-    @Override
-    public void onPacketPlayReceive(PacketPlayReceiveEvent event) {
-        var player = event.getPlayer();
 
-        if (event.isCancelled()) {
-            return;
-        }
 
-        if (!plugin.isPlayerWithSaver(player.getUniqueId())) {
-            return;
-        }
-
-        if (event.getPacketId() == PacketType.Play.Client.SETTINGS) {
-            var packet = new WrappedPacketInSettings(event.getNMSPacket());
-            packet.setViewDistance(3);
-        }
+    private void startMapClearSchedule() {
+        Bukkit.getScheduler().runTaskTimer(plugin, cartCounters::clear, 20 * 120, 20 * 120);
     }
-
-     */
 }
